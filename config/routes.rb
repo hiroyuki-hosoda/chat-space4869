3ec7b8972d2a795c73a87221b026eba63dc571a9
hiroyuki-hosoda <antausotsukidane45@gmail.com>
2020-09-26 14:28:53 +0900

    デバイス導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+<<<<<<< Updated upstream
+  
+  root "messages#index"
+  resources :users, only: [:edit, :update]
+
+=======
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+>>>>>>> Stashed changes
 end
